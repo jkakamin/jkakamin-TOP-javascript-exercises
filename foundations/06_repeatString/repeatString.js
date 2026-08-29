88e22ccbd99fe356b2c8220d0ea6475c62d839bb
jkakamin <11204856+jkakamin@users.noreply.github.com>
2026-08-29 13:45:09 -1000

    Complete foundations 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let newStr = '';
 
+    if (num < 0){newStr = 'ERROR'}
+    else{
+        for(i = 0; i < num; i++){
+            newStr += str;
+        }
+    }
+    return newStr
 };
 
 // Do not edit below this line
