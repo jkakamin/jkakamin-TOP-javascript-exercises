ad39f2690b6f504df8c4792d90da83e863c8a297
jkakamin <11204856+jkakamin@users.noreply.github.com>
2026-08-30 23:48:34 -1000

    Complete foundations 08
    Required research.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, a, b, c, d) {
+    let remove = [a, b, c, d];
+    let newArr = [];
+
+    return arr
+        .filter( (element) =>
+            ! remove.includes(element)
+        )
+
+    
 };
 
 // Do not edit below this line
