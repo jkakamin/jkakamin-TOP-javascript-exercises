45c99fd657667b76e14e2d7b9dc84c7f8d9ca535
jkakamin <11204856+jkakamin@users.noreply.github.com>
2026-09-01 13:16:15 -1000

    Complete foundations 09
    Nominal success. Used an overcomplicated method utilizing arrays.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    const inputs = [a, b];
+    let allInts = [];
+    let sumInts;
+
+    inputs.sort( (a, b) => a - b )
+    
+    for (let i = inputs[0]; i <= inputs[1]; i++){
+        allInts.unshift(i);
+    }
+
+    sumInts =
+        typeof(a) !== 'number' || typeof(b) !== 'number'?
+        'ERROR':
+        a >= 0 && b >=0 && ((a + b) % 1) === 0 ?
+        allInts.reduce( (total, curr) => total + curr ):
+        'ERROR';
+    
+    return sumInts;
+
 
 };
 
