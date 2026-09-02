1391d49405b8bc8de78e2370434ffd4f1f06802e
jkakamin <11204856+jkakamin@users.noreply.github.com>
2026-09-01 14:16:02 -1000

    Complete foundations 10
    Solution used more elegant and maintainable code, defines rules as booleans
    I use direct definition of rules in the if statement, less maintainable.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,4 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    //leap year if div by 4, not div by 100, div by 400
+    //div 4 = all leap year candidates
+    //not div 100 && div 400? leap year
+    //
+
+    if(year % 4 == 0){
+        if(year % 100 == 0 && year % 400 == 0) return true;
+        else if(year % 100 == 0 ) return false;
+        else return true;
+    } else return false;
+
 
 };
 
