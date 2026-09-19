6653e5e5d931cfd917cd92737b3e2eb58295f1b6
jkakamin <11204856+jkakamin@users.noreply.github.com>
2026-09-18 22:19:16 -1000

    Complete foundations 12
    For power function, forgot the existence of the exponent operator **, instead
    used for loop to repeatedly multiply and hard-coded results for powers of 0 and 1.
    For factorial, chose to use for loop with starting value 1 and increment up
    to the number input argument instead of decrementing from input as in solution.

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, curr) => acc + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.length === 0? 0:
+    arr.reduce((acc, curr) => acc * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(num, exponent) {
+	let acc = 1;
+  for(let i = 0; i < exponent; i++){
+    acc = acc * num;
+  };
+
+  return exponent === 0? 1:
+    exponent === 1? num:
+    acc;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let acc = 1;
+  for(let i = 1; i <= num; i++){
+    acc = acc * i;
+  };
+
+  return acc;
 };
 
 // Do not edit below this line
