a60d5f21bf2a5250c76a9c55a70dec5cc8393d4f
jkakamin <11204856+jkakamin@users.noreply.github.com>
2026-09-18 23:55:08 -1000

    Complete foundations 13
    Significant difference from solution. Defined an array of characters to remove,
    then iterate through with for loop to split and rejoin original string. In
    hindsight defining only valid characters is more sustainable code.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let removeChars = [' ', ',', '.', '!', '?', ];
+    
+    let letters = str.toLowerCase();
 
+    for(let i = 0; i < removeChars.length; i++){
+        letters = letters
+            .split(removeChars[i])
+            .join('')
+        ;
+    };
+
+    let reverseLetters = letters
+        .split('')
+        .reverse()
+        .join('')
+    ;
+
+    return letters === reverseLetters;
 };
 
 // Do not edit below this line
